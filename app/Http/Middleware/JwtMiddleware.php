bda3ec46a5150f931ed569de3e713f2b1eb08c8c
anasmoundir <anasmoundir1996@gmail.com>
2023-03-20 16:50:38 +0000

    a

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class JwtMiddleware extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next   
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        try{
+            $user =JWTAuth::parseToken()->authenticate();
+        }catch(exeption $e){
+            if($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+                return response()->json(['status'=>'Token is Invalid']);
+            }else if($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+                return response()->json(['status'=>'Token is Expired']);
+            }else{
+                return response()->json(['status'=>'Authorization Token not found']);
+            }
+        }
+
+        return $next($request);
+    }
+}
